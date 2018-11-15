aca4f516db2e71c11fe4dc5e220ca969bd4ff1a0
Jaydip Barvaliya <40881840+jaydip13189@users.noreply.github.com>
2018-11-15 15:18:19 +0530

    .

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
+// Client ID:2b3a13c32f27df9
+// Client secret: fca5241c2b1884485b2e77b7f2a1a655056b3900
+
+
 <template>
     <AppHeader></AppHeader>
 </template>
 
 <script>
-import AppHeader from './components/AppHeader';
+import AppHeader from "./components/AppHeader";
 export default {
-    name: 'App',
-    components:{
-        AppHeader
-    }
+  name: "App",
+  components: {
+    AppHeader
+  }
 };
 </script>
 
