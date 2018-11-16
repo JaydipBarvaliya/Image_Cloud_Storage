c4027ed9ab18f66f5b4fa092a9e7e590cd1904f8
Jaydip Barvaliya <40881840+jaydip13189@users.noreply.github.com>
2018-11-16 14:39:43 +0530

    .

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,9 +7,9 @@
   
    
   <div class="right menu">
-    <a href="#" class="ui item" @click="login">
-      Log In
-    </a>
+    <a href="#" class="ui item" @click="login">Log In</a>
+    <a href="#" class="ui item" @click="logout">LogOut</a>
+    
   </div>
 </div> 
 
@@ -19,6 +19,6 @@
 import { mapActions } from "vuex";
 export default {
   name: "AppHeader",
-  methods: mapActions(["login"])
+  methods: mapActions(["login"],["logout"])
 };
 </script>
